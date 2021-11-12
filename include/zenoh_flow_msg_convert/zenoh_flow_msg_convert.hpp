a1d6214d8bef56efe8d44600d1398015ff505438
moelang <luxiang.zhou@autocore.ai>
2021-11-12 16:26:56 +0800

    add pure pursuit (#6)

--- include/zenoh_flow_msg_convert/zenoh_flow_msg_convert.hpp
+++ include/zenoh_flow_msg_convert/zenoh_flow_msg_convert.hpp
@@ -1,11 +1,17 @@
 #pragma once
 #include <autoware_auto.hpp>
-#include <std_msgs/msg/header.hpp>
+#include <autoware_auto_msgs/msg/trajectory.hpp>
+#include <autoware_auto_msgs/msg/vehicle_control_command.hpp>
+#include <autoware_auto_msgs/msg/vehicle_kinematic_state.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <geometry_msgs/msg/quaternion.hpp>
+#include <std_msgs/msg/header.hpp>
 using namespace zenoh_flow::autoware_auto::ffi;
 
+AutowareAutoMsgsTrajectory Convert(const autoware_auto_msgs::msg::Trajectory &);
+AutowareAutoMsgsVehicleControlCommand Convert(const autoware_auto_msgs::msg::VehicleControlCommand &);
+AutowareAutoMsgsVehicleKinematicState Convert(const autoware_auto_msgs::msg::VehicleKinematicState &);
 BuiltinInterfacesTime Convert(const builtin_interfaces::msg::Time &);
 GeometryMsgsPoint Convert(const geometry_msgs::msg::Point &);
 GeometryMsgsPose Convert(const geometry_msgs::msg::Pose &);
@@ -14,6 +20,9 @@ GeometryMsgsPoseWithCovariance Convert(const geometry_msgs::msg::PoseWithCovaria
 GeometryMsgsPoseWithCovarianceStamped Convert(const geometry_msgs::msg::PoseWithCovarianceStamped &);
 GeometryMsgsQuaternion Convert(const geometry_msgs::msg::Quaternion &);
 StdMsgsHeader Convert(const std_msgs::msg::Header &);
+autoware_auto_msgs::msg::Trajectory Convert(const AutowareAutoMsgsTrajectory &);
+autoware_auto_msgs::msg::VehicleControlCommand Convert(const AutowareAutoMsgsVehicleControlCommand &);
+autoware_auto_msgs::msg::VehicleKinematicState Convert(const AutowareAutoMsgsVehicleKinematicState &);
 builtin_interfaces::msg::Time Convert(const BuiltinInterfacesTime &);
 geometry_msgs::msg::Point Convert(const GeometryMsgsPoint &);
 geometry_msgs::msg::Pose Convert(const GeometryMsgsPose &);
--- include/zenoh_flow_pure_pursuit/zenoh_flow_pure_pursuit.hpp
+++ include/zenoh_flow_pure_pursuit/zenoh_flow_pure_pursuit.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <pure_pursuit_nodes/pure_pursuit_node.hpp>
 #include <autoware_auto.hpp>
 
 namespace zenoh_flow
@@ -9,6 +10,14 @@ namespace zenoh_flow
         {
             class PurePursuit
             {
+            public:
+                PurePursuit(const CfgPurePursuit &);
+                AutowareAutoMsgsVehicleControlCommand GetControlCmd();
+                void SetTrajectory(const AutowareAutoMsgsTrajectory &);
+                void SetKinematicState(const AutowareAutoMsgsVehicleKinematicState &);
+
+            private:
+                std::shared_ptr<autoware::motion::control::pure_pursuit_nodes::PurePursuitNode> ptr;
             };
             AutowareAutoMsgsVehicleControlCommand pure_pursuit_get_control_cmd(std::unique_ptr<PurePursuit> &);
             std::unique_ptr<PurePursuit> pure_pursuit_init(const CfgPurePursuit &);
